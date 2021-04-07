54444d2bf988660cc77fa75f107452ed58b522c3
Mert Saraç <mertsarac@yandex.com>
2021-04-07 08:57:00 +0300

    created empty project

---
+++ ios-templates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-templates
+//
+//  Created by Mert Saraç on 7.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
